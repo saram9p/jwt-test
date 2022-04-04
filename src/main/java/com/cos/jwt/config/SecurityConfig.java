315bd9b39532288252eb18db69efae8094460f7b
saram9p <saram9p@gmail.com>
2022-04-04 21:26:22 +0900

    security 설정

---
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.cos.jwt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration // ioc 할 수 있게 만듬
+@EnableWebSecurity // 시큐리티 활성화
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	private final CorsFilter corsFilter;
+	
+@Override
+protected void configure(HttpSecurity http) throws Exception {
+	http.csrf().disable();
+	http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않겠다는 것(stateless 서버로 만듬)
+	.and()
+	.addFilter(corsFilter) // 모든 요청은 corsFilter를 탄다, @CrossOrigin(인증X), 시큐리티 필터에 등록 인증(O)
+	.formLogin().disable()
+	.httpBasic().disable()
+	.authorizeRequests()
+	.antMatchers("api/v1/user/**")
+	.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+	.antMatchers("api/v1/manager/**")
+	.access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+	.antMatchers("api/v1/admin/**")
+	.access("hasRole('ROLE_ADMIN')")
+	.anyRequest().permitAll(); // 다른 요청은 전부 권한없이 들어갈 수 있다.
+}
+}
