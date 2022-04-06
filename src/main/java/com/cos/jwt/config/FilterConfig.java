7d7bf8e6805935993a2b57e984580544e7b019d9
saram9p <saram9p@gmail.com>
2022-04-06 19:52:37 +0900

    jwt Filter 등록 테스트

---
+++ src/main/java/com/cos/jwt/config/FilterConfig.java
@@ -0,0 +1,28 @@
+package com.cos.jwt.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.cos.jwt.filter.MyFilter1;
+import com.cos.jwt.filter.MyFilter2;
+
+@Configuration // IOC에 등록하면 메모리에 뜬다.
+public class FilterConfig {
+
+	@Bean
+	public FilterRegistrationBean<MyFilter1> filter1(){
+		FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
+		bean.addUrlPatterns("/*");
+		bean.setOrder(1); // 낮은 번호가 필터중에서 가장 먼저 실행됨.
+		return bean;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<MyFilter2> filter2(){
+		FilterRegistrationBean<MyFilter2> bean = new FilterRegistrationBean<>(new MyFilter2());
+		bean.addUrlPatterns("/*");
+		bean.setOrder(0); // 낮은 번호가 필터중에서 가장 먼저 실행됨.
+		return bean;
+	}
+}
---
+++ src/main/java/com/cos/jwt/filter/MyFilter3.java
@@ -0,0 +1,20 @@
+package com.cos.jwt.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class MyFilter3 implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		System.out.println("필터3");
+		chain.doFilter(request, response); // 필터에 걸리는 순간 끝나지 않게 하기위해 체인에다 넘겨준다.
+	}
+
+}
