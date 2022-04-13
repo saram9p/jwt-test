5c2ae1b0e9610c03970c5c177dddac3027cd50bb
saram9p <saram9p@gmail.com>
2022-04-13 21:16:31 +0900

    ﻿jwt를 위한 강제 로그인 진행(1)

--- src/main/java/com/cos/jwt/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/cos/jwt/config/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,8 @@
 package com.cos.jwt.config.jwt;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,6 +10,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.cos.jwt.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,6 +30,20 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		System.out.println("JwtAuthenticationFilter : 로그인 시도중");
 		
 		// 1. username, password를 받아서
+		try {
+//			BufferedReader br = request.getReader();
+//			
+//			String input = null;
+//			while((input = br.readLine()) != null) {
+//				System.out.println(input);
+//			}
+			ObjectMapper om = new ObjectMapper(); // 이 클래스는 json 데이터를 파싱해준다.
+			User user = om.readValue(request.getInputStream(), User.class);
+			System.out.println(user);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("===============================");
 		
 		// 2. 정상인지 로그인 시도를 해보는 것, authenticationManager로 로그인을 시도하면!
 		// PrincipalDetailsService가 호출, loadUserByUsername() 함수 실행됨.
